af446a0e34e1d675e8c343ef56d52dcb852edde0
Syeda Hira Rizvi <rizvisyedahira@gmail.com>
2026-02-22 16:37:28 +0500

    removing one import from user.js inside controller folder

--- backend/controller/user.js
+++ backend/controller/user.js
@@ -8,7 +8,7 @@ const jwt = require("jsonwebtoken");
 const sendMail = require("../utils/sendMail");
 const sendToken = require("../utils/jwtToken");
 const { isAuthenticated, isAdmin } = require("../middleware/auth");
-const { server } = require("../../frontend/src/server");
+// const { server } = require("../../frontend/src/server");
 
 // create user
 router.post("/create-user", async (req, res, next) => {
