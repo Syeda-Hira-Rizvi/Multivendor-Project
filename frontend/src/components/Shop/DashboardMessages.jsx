78dfc2436da25418b8164ebcd1e0f0de1c37a67f
Syeda Hira Rizvi <rizvisyedahira@gmail.com>
2026-02-27 18:59:07 +0500

    added production url or socket in frontend

--- frontend/src/components/Shop/DashboardMessages.jsx
+++ frontend/src/components/Shop/DashboardMessages.jsx
@@ -10,7 +10,7 @@ import { TfiGallery } from "react-icons/tfi";
 import socketIO from "socket.io-client";
 import { format } from "timeago.js";
 // const ENDPOINT = "https://socket-ecommerce-tu68.onrender.com/";
-const ENDPOINT = "http://localhost:4000";
+const ENDPOINT = "https://multivendor-project-socket.onrender.com";
 const socketId = socketIO(ENDPOINT, { transports: ["websocket"] });
 
 const DashboardMessages = () => {
